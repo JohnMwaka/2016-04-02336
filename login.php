a8ee8b02887a62f0003748c826d857eed394082f
JohnMwaka <johnmwaka.70@gmail.com>
2017-06-13 11:59:05 +0300

    Create login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST['submit'])){
+if(isset($_POST['login'])){
     
     echo "<script>alert('SIGN IN SUCCESSFUL. Click OK to CONTINUE')</script>";
     echo "<script>window.open('home.php','_self')</script>";
