89a75ede5b0642272a717d3485ac5db7ea232173
JohnMwaka <johnmwaka.70@gmail.com>
2017-06-13 11:54:08 +0300

    Create sign_up_action.php

--- sign_up_action.php
+++ sign_up_action.php
@@ -3,6 +3,6 @@
 if(isset($_POST['sign_up'])){
     
     echo "<script>alert('REGISTRATION SUCCESSFUL. Click OK to CONTINUE.')</script>";
-    echo "<script>window.open('welcome.php')</script>";
+    echo "<script>window.open('welcome.php','_self')</script>";
     
 }
